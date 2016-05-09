e678cfd9daa858ffbc6e2d6ba14c24182ff331bc
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-05-09 15:20:27 -0500

    C send thumbnail weight/height and get url from config.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,23 +2,18 @@ class MainController < ApplicationController
   
   def index
     time = Time.new
-    
-    uri = "http://mx.tuhistory.com/hoy-en-la-historia"
-    date = "2015-#{@date_month}-#{@date_day}"
-    doc = Nokogiri::HTML(open("#{uri}/#{date}"))
     @date_day = params[:d].to_s.rjust(2, '0') || time.day.to_s.rjust(2, '0')
     @date_month = params[:m].to_s.rjust(2, '0') || time.month.to_s.rjust(2, '0')
     
+    doc = Nokogiri::HTML(open("#{Rails.application.config.history_api_url}/#{Time.now.year}-#{@date_month}-#{@date_day}"))
+
     @days = []
     doc.search(".contenido").each do |t|
         dTitle = t.search("h4 a").text
         dDate = t.search("h6").text
-        dUrl = t.search(".content-image img").attr('src').to_s
-        day = Day.new(dTitle, dDate, dUrl)
-        @days << day
-        
-        puts day.url
-    
+        dImage = Image.new(t.search(".content-image img"))
+
+        @days << Day.new(dTitle, dDate, dImage)
     end
     
     respond_to do |format|
@@ -30,17 +25,30 @@ class MainController < ApplicationController
 end
 
 class Day
-  attr_accessor :title, :date, :url
-    def initialize(title, date, url)
+  attr_accessor :title, :date, :thumb
+    def initialize(title, date, thumb)
        @title = title
        @date = date
-       @url = url
-       
+       @thumb = thumb
     end
     
     def to_s
-       "#{@title} - #{@date} - #{@url}"
+       "#{@title} - #{@date} - #{image}"
+    end
+end
+
+class Image
+  attr_accessor :src, :w, :h
+    def initialize(img)
+       @src = img.attr('src').to_s
+       @w = img.attr('width').to_s.to_i
+       @h = img.attr('height').to_s.to_i
+
+       @src.strip!
     end
 
+     def to_s
+       "#{@w} - #{@h} - #{@src}"
+    end
 end
 
