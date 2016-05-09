fe2d363035b2a4ca168176061298b0f3dd0c9715
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-05-09 14:54:51 -0500

    Add compression

--- Gemfile
+++ Gemfile
@@ -45,6 +45,5 @@ group :development do
 end
 
 group :production do
-  gem 'pg'
   gem 'rails_12factor'
 end
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module DayInHistory
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Gzip compression  
+    config.middleware.use Rack::Deflater
   end
 end
